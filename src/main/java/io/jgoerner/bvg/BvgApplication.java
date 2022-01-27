875777034870569f84ca2c99b182c307feb0dc0a
jgoerner <joshua.goerner@gmail.com>
2022-01-27 11:36:03 +0100

    :tada: Add bootstrapped code skeleton

---
+++ src/main/java/io/jgoerner/bvg/BvgApplication.java
@@ -0,0 +1,13 @@
+package io.jgoerner.bvg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BvgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jgoerner/bvg/BvgApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jgoerner.bvg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
