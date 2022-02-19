72a42e206eab31aab61b9da902ce3f6baba6e320
jgoerner <joshua.goerner@gmail.com>
2022-02-19 08:33:08 +0100

    :fire: Remove duplicated port
    Signed-off-by: jgoerner <joshua.goerner@gmail.com>

--- src/main/java/io/jgoerner/bvg/application/port/out/GetShortestRouteBetweenStations.java
+++
@@ -1,8 +0,0 @@
-package io.jgoerner.bvg.application.port.out;
-
-import io.jgoerner.bvg.domain.Route;
-
-public interface GetShortestRouteBetweenStations {
-    Route getShortestRouteBetweenStations(String from, String to);
-
-}
