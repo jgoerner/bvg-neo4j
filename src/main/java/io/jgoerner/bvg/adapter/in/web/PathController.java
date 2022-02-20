ee5cfb9b1ceb7af0563bf028eb4e630ce4977804
jgoerner <joshua.goerner@gmail.com>
2022-02-20 07:03:47 +0100

    :sparkles: Add REST controller to retrieve shortest route
    Signed-off-by: jgoerner <joshua.goerner@gmail.com>

---
+++ src/main/java/io/jgoerner/bvg/adapter/in/web/PathController.java
@@ -0,0 +1,25 @@
+package io.jgoerner.bvg.adapter.in.web;
+
+
+import io.jgoerner.bvg.application.port.out.RetrieveSimplePath;
+import io.jgoerner.bvg.domain.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/route")
+public class PathController {
+
+    @Autowired
+    private RetrieveSimplePath simplePathRetriever;
+
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public Route test(@RequestParam("from") String from, @RequestParam("to") String to) {
+        // CQRS-ish shortcut straight to out ports
+        return simplePathRetriever.retrieveSimplePath(from, to);
+    }
+}
