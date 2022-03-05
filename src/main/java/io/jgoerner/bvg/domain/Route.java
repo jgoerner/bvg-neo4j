77f902e8e6b7d0d0d05ab1923d2067c0d8ecf4e1
jgoerner <joshua.goerner@gmail.com>
2022-03-05 23:32:02 +0100

    :necktie: Add route with summary type
    Signed-off-by: jgoerner <joshua.goerner@gmail.com>

--- src/main/java/io/jgoerner/bvg/domain/Route.java
+++ src/main/java/io/jgoerner/bvg/domain/Route.java
@@ -3,4 +3,13 @@ package io.jgoerner.bvg.domain;
 import java.util.List;
 
 public record Route(List<Segment> segments) {
+    public RouteWithSummary withSummary() {
+        return new RouteWithSummary(
+                this.segments,
+                this.segments.size() > 0 ? this.segments.size() + 1 : 0,
+                this.segments.stream().mapToInt(Segment::duration).sum());
+    }
+}
+
+record RouteWithSummary(List<Segment> segments, Integer stations, Integer duration) {
 }
