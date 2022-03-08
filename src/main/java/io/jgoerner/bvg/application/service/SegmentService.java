354f41db80e703a560a96bf66549e15aede4aa39
jgoerner <joshua.goerner@gmail.com>
2022-03-08 23:47:28 +0100

    :fire: Remove unused logger
    Signed-off-by: jgoerner <joshua.goerner@gmail.com>

--- src/main/java/io/jgoerner/bvg/application/service/SegmentService.java
+++ src/main/java/io/jgoerner/bvg/application/service/SegmentService.java
@@ -1,17 +1,14 @@
 package io.jgoerner.bvg.application.service;
 
 import io.jgoerner.bvg.application.port.in.CreateSegment;
-import io.jgoerner.bvg.application.port.out.SaveSegment;
+import io.jgoerner.bvg.application.port.out.segment.SaveSegment;
 import io.jgoerner.bvg.domain.Segment;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SegmentService implements CreateSegment {
 
-    Logger log = LoggerFactory.getLogger(SegmentService.class);
 
     @Autowired
     private SaveSegment segmentSaver;
